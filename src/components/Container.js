4bb22d76ec6c50f87e83feb8352ca5f3fdf4d89f
Alex Falcone <65960268+sokolowav@users.noreply.github.com>
2023-01-18 21:44:51 +0300

    technical deploy

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,6 +4,7 @@ import { Card } from './Card'
 export const Container = () => {
   return (
     <div className='container'>
+      <span className='container__heading'>Ты сегодня покормил кота?</span>
       <Card></Card>
 
       {/* <div class='box'></div> */}
