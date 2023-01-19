bb3c9702d1a5460aa5e5819c980f50386069978a
Alex Falcone <65960268+sokolowav@users.noreply.github.com>
2023-01-19 22:27:16 +0300

    technical deploy

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import { data } from '../data'
 
-export const Card = () => {
+export const Card = (props) => {
   return (
     <div className='card'>
       <div className='card__box'>
         <div className='card__content'>
           <span className='card__content-title'>Сказочное заморское яство</span>
           <h1 className='card__content-name'>Нямушка</h1>
-          <h1 className='card__content-taste'>с фуа-гра</h1>
-          <span className='card__content-amount'>10 порций</span>
-          <span className='card__content-bonus'>мышь в подарок</span>
-          <span className='card__content-result'></span>
+          <h1 className='card__content-taste'>{data[0].taste}</h1>
+          <span className='card__content-amount'>{data[0].amount}</span>
+          <span className='card__content-bonus'>{data[0].bonus}</span>
+          <span className='card__content-result'>{data[0].result}</span>
         </div>
       </div>
       <div className='card__caption'>
